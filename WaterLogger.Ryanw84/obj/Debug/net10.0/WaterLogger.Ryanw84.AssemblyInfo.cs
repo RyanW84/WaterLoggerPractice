601dfc45d06579040fc5954835b5a3048ca62175
Ryan Weavers <ryanweavers@gmail.com>
2026-01-21 18:08:16 +0000

    DB Created and beginning to build index page

--- WaterLogger.Ryanw84/obj/Debug/net10.0/WaterLogger.Ryanw84.AssemblyInfo.cs
+++ WaterLogger.Ryanw84/obj/Debug/net10.0/WaterLogger.Ryanw84.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterLogger.Ryanw84")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc4000b6d5b024f160b1f2ee03b2295329cca54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3c243f81b590a5200474f38ac8d577ef4a338d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterLogger.Ryanw84")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterLogger.Ryanw84")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
