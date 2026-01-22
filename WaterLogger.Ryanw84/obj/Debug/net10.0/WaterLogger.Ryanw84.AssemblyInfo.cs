871c593f437526faa6b746dbb5be471f8e4eaa6b
Ryan Weavers <ryanweavers@gmail.com>
2026-01-22 21:18:37 +0000

    quick push

--- WaterLogger.Ryanw84/obj/Debug/net10.0/WaterLogger.Ryanw84.AssemblyInfo.cs
+++ WaterLogger.Ryanw84/obj/Debug/net10.0/WaterLogger.Ryanw84.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterLogger.Ryanw84")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c1de915a0b19d2fc216164c3a9abf28b8d6d93b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24cdd135f5b2c7a8faba7cb1e51c774f6cf5ee82")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterLogger.Ryanw84")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterLogger.Ryanw84")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
